4b8617c385ded8a2e8c2348f0c6d71c94c178912
MrMasterplan <simon@heisterkamp.dk>
2011-06-05 12:05:58 +0200

    added test suite

---
+++ Testing/Full_Cycle_Test/VariableList.C
@@ -0,0 +1,8 @@
+// This is a comment
+// comments are excluded as long as they don't look like declarations.
+//UInt_t RunNumber;
+int i;
+//float f;
+vector<float> *v;/*
+vector<float> *el_eta;
+vector<float> *el_phi;*/
