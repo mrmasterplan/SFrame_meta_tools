80f079c0361ac25054c5a972b213ac8f097cff77
MrMasterplan <simon@heisterkamp.dk>
2011-10-17 13:58:31 +0200

    changed setup.sh to be more python and less bash

--- setup.sh
+++ setup.sh
@@ -1,21 +1,15 @@
-
 #This needs to be sourced.
 
 
-previous_PWD=$PWD
-previous_OLDPWD=$OLDPWD
+# find out where this script is located.
+# this line is what allows you to source this script from anywhere
 
-cd $(dirname $BASH_SOURCE)
-SFrame_meta_tool_dir="$(pwd)"
-cd $previous_PWD
-OLDPWD=$previous_OLDPWD
-
-unset previous_PWD
-unset previous_OLDPWD
+SFrame_meta_tool_dir=$(python -c "from os.path import *; print abspath(expanduser(dirname('$BASH_SOURCE')))")
 
 #Set up so that I can use the tools:
 
 export PATH="${SFrame_meta_tool_dir}/bin:${PATH}"
 export PYTHONPATH="${SFrame_meta_tool_dir}/python:${PYTHONPATH}"
 
+#we don't want this to hand around:
 unset SFrame_meta_tool_dir
