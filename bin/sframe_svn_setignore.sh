cd620840e500de5b7a2e529fc1872448c4179442
MrMasterplan <simon@heisterkamp.dk>
2012-10-10 11:51:38 +0200

    added script to set standard svn ignores.

---
+++ bin/sframe_svn_setignore.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+function sframe_svn_setignore {
+    local LIBRARY="$(grep LIBRARY Makefile | sed 's/LIBRARY *=* *//g')"
+    local NL=$'\n'
+
+    local PREV="$(svn propget svn:ignore .)"
+    svn propset svn:ignore "${PREV}${NL}obj${NL}.sframe.\*" .
+    local PREV="$(svn propget svn:ignore src)"
+    svn propset svn:ignore "${PREV}${NL}${LIBRARY}_Dict.h${NL}${LIBRARY}_Dict.cxx${NL}_${LIBRARY}_version_info.cxx" src
+
+    echo "Now ignoring:"
+    echo "   obj/"
+    echo "   .sframe.\*"
+    echo "   src/${LIBRARY}_Dict.h"
+    echo "   src/${LIBRARY}_Dict.cxx"
+    echo "   src/_${LIBRARY}_version_info.cxx"
+    echo "If you have trouble checking in please run \'svn up\' first."
+}
+
+sframe_svn_setignore
+
+unset sframe_svn_setignore
